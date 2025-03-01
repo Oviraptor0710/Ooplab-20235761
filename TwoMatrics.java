24342c55f2291bab627678ccf261681e14c5ee29
Oviraptor0710 <kien07102005@gmail.com>
2025-03-01 20:46:10 +0700

    lab01_6.6

--- TwoMatrics.java
+++ TwoMatrics.java
@@ -4,19 +4,13 @@ public class TwoMatrics {
         Scanner sc = new Scanner(System.in);
         int rows = sc.nextInt();
         int cols = sc.nextInt();
-        
         int[][] matrix1 = new int[rows][cols];
         int[][] matrix2 = new int[rows][cols];
         
         inputMatrix(sc, matrix1, rows, cols);     // Nhập ma trận thứ nhất   
         inputMatrix(sc, matrix2, rows, cols);     // Nhập ma trận thứ hai
-        
-        // Hiển thị hai ma trận
-        printMatrix(matrix1, rows, cols);
-        printMatrix(matrix2, rows, cols);
-                sc.close();
-    }
-    
+        addTwoMatrix(matrix1, matrix2, rows, cols);
+    }        
     // Hàm nhập ma trận
     public static void inputMatrix(Scanner sc, int[][] matrix, int rows, int cols) {
         for (int i = 0; i < rows; i++) {
@@ -25,12 +19,19 @@ public class TwoMatrics {
             }
         }
     }
-    
-    // Hàm in ma trận
-    public static void printMatrix(int[][] matrix, int rows, int cols) {
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                System.out.print(matrix[i][j] + " ");
+
+
+    // Hàm cộng 2 ma trận
+    public static void addTwoMatrix(int [][]a, int [][]b, int rows, int cols){
+        int [][]ans = new int[rows][cols];
+        for (int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                ans[i][j] = a[i][j] + b[i][j];
+            }
+        }
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                System.out.print(ans[i][j] + " ");
             }
             System.out.println();
         }
